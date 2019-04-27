bf90f79782b79724c410e1d99d360335f844b84e
RICARDO PRESILLA <ricardopresilla@gmail.com>
2019-04-27 13:27:25 -0430

    Iniciando el proyecto.

---
+++ ReproductorVideos/src/reproductorvideos/ReproductorVideos.java
@@ -0,0 +1,21 @@
+/*
+ * ReproductorVideos.java
+ * Licencia GNU.
+ */
+package reproductorvideos;
+
+/**
+ * Programa para reproducir videos, utilizando la libreria JMF.
+ * @author Ingeniero en Computación Ricardo Presilla
+ * @version 1.0.
+ */
+public class ReproductorVideos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
